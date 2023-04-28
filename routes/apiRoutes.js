2dd2f6af231c3530783cfeb8650508c43d4f2cf8
Dan <123746582+DanielFMcKay@users.noreply.github.com>
2023-04-28 00:34:14 -0700

    Full functionality reached. Also, Tera Copy was being fickle and didn't copy stuff into my homework folder correctly, so gotta re-push.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,27 +5,27 @@ const path = require('path');
 var uniqid = require('uniqid');
 
 
-// routing
-module.exports = (app) => {
+// routing stuff
+module.exports = (route) => {
 
   // GET /api/notes should read the db.json file and return all saved notes as JSON.
-  app.get('/api/notes', (req, res) => {
+  route.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../db/db.json'));
   });
 
   // POST /api/notes should receive a new note to save on the request body, 
   // add it to the db.json file, and then return the new note to the client. 
-  app.post('/api/notes', (req, res) => {
+  route.post('/api/notes', (req, res) => {
     let db = fs.readFileSync('db/db.json');
     db = JSON.parse(db);
     res.json(db);
 
-    // creating body for a new note
+    // creating the body for a new note
     let userNote = {
       title: req.body.title,
       text: req.body.text,
 
-      // assigning a unique id for each new note
+      // Uniqid assigns a unique id for each new note. Kinda straightforward tbh.
       id: uniqid(),
     };
     // pushing created note to be written in the db.json file
@@ -36,13 +36,13 @@ module.exports = (app) => {
   });
 
 
-  // DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete.
-  app.delete('/api/notes/:id', (req, res) => {
-    // reading notes form db.json
+  // Deleting Notes: /api/notes/:id receives the id for the note to delete. In theory.
+  route.delete('/api/notes/:id', (req, res) => {
+    // reading note fromdb.json
     let db = JSON.parse(fs.readFileSync('db/db.json'))
     // removing note with id
     let deleteNote = db.filter(item => item.id !== req.params.id);
-    // Rewriting note to db.json
+    // Overwriting note to db.json
     fs.writeFileSync('db/db.json', JSON.stringify(deleteNote));
     res.json(deleteNote);
     
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,15 @@
 const path = require('path');
 
 // routing
-module.exports = (app) => {
+module.exports = (route) => {
 
   // Routes to notes.html via GET '/notes'
-  app.get('/notes', (req, res) => {
+  route.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
   });
 
   // Routes back to the index.html page
-  app.get('*', (req, res) => {
+  route.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
   })
 };
